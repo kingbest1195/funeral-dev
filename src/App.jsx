92dd4a53f92b8c5cffca6a04699f6f717fe6fe5f
kingbest1195 <kingbest1195@gmail.com>
2025-09-20 19:32:20 +0330

    404-page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import HomePage from './pages/HomePage/HomePage.jsx';
 import PrivacyPage from './pages/PrivacyPage/PrivacyPage.jsx';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage.jsx';
 import StructuredData from './components/StructuredData/StructuredData.jsx';
 import './styles/main.scss';
 
@@ -15,6 +16,8 @@ const App = () => {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/privacy" element={<PrivacyPage />} />
+            {/* Catch-all route для 404 */}
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </div>
       </Router>
